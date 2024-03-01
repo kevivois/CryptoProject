4ce44dace7d1440b60989e4dd1ed5d8c3cdbdf1c
kevin voisin <kevin.voisin187@gmail.com>
2024-03-01 18:58:05 +0100

    cleaned code

--- Main.py
+++ Main.py
@@ -1,33 +1,48 @@
 import time
-from threading import Thread
+from Socket import MySocket
 
-import Socket
 
-b = Socket.MySocket()
+def get_user_input(prompt):
+    return input(prompt).strip()
 
-b.connect("vlbelintrocrypto.hevs.ch", 6000)
-#lgth = b.sendshift("quoicoõubeh !", 't')
 
-while True:
-    cmd = input("Write your command ")
-    shift = input("Do you want to encode the message ? [no|yes]")
-    messageType = input("Specify your message type [t:@everyone|s:server only]")
-    encodeMessage = True if "yes" in shift else False
-    messageType = "t" if "t" in messageType else "s"
-    if encodeMessage:
-        typeEncoding = 1 if "shift" in cmd.split(" ")[1] else 2
-        typeEncoding = int(typeEncoding)
-        b.send(cmd,messageType)
-        msg = b.receive('s')
-        encodeLength = int(msg.split(" ")[-1])
-        msgToEncode = b.receive('s')
-        if typeEncoding == 1:
-            b.sendshift(msgToEncode,messageType,encodeLength)
-        elif typeEncoding == 2:
-            b.sendxor(msgToEncode,messageType,encodeLength)
-        response = b.receive('s')
-        print(response)
-    else:
-        b.send(cmd,messageType)
-        response = b.receive(messageType)
-        print(response)
+def encode_message(b, cmd, messageType):
+    type_encoding = 1 if "shift" in cmd.split(" ")[1] else 2
+    type_encoding = int(type_encoding)
+    b.send(cmd, messageType)
+
+    msg = b.receive('s')
+    encode_length = int(msg.split(" ")[-1])
+    msg_to_encode = b.receive(messageType)
+
+    if type_encoding == 1:
+        b.sendshift(msg_to_encode, messageType, encode_length)
+    elif type_encoding == 2:
+        b.sendxor(msg_to_encode, messageType, encode_length)
+
+    response = b.receive(messageType)
+    print(response)
+
+
+def main():
+    b = MySocket()
+    b.connect("vlbelintrocrypto.hevs.ch", 6000)
+
+    while True:
+        cmd = get_user_input("Write your command: ")
+        shift = get_user_input("Do you want to encode the message? [no|yes]: ")
+        message_type = get_user_input("Specify your message type [t:@everyone|s:server only]: ")
+
+        encode_message_flag = "yes" in shift
+        message_type = "t" if "t" in message_type else "s"
+
+        if encode_message_flag:
+            encode_message(b, cmd, message_type)
+        else:
+            b.send(cmd, message_type)
+            response = b.receive(message_type)
+            print(response)
+
+
+if __name__ == "__main__":
+    main()
--- Socket.py
+++ Socket.py
@@ -1,19 +1,15 @@
 import socket
 
-
 class MySocket:
 
     def __init__(self, sock=None):
         if sock is None:
-            self.sock = socket.socket(
-                socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
 
     def connect(self, host, port):
         self.sock.connect((host, port))
-        print("is connected")
-
     def send(self, msg, messageType):
         payload = bytes("ISC", 'utf-8') + bytes(messageType, 'utf-8')
         payload += len(msg).to_bytes(2, byteorder='big')
@@ -26,10 +22,13 @@ class MySocket:
     def sendshift(self, msg: str, messageType: str, amount: int):
         payload = bytes("ISC", 'utf-8') + bytes(messageType, 'utf-8')
         payload += len(msg).to_bytes(2, byteorder='big')
+        shifted_message = ""
         for p in msg:
-            v = (int.from_bytes(bytes(p,"utf-8"))+amount).to_bytes(4)
+            v = (int.from_bytes(bytes(p, "utf-8")) + amount).to_bytes(4)
+            shifted_message += chr(int.from_bytes(bytes(p, "utf-8")) + amount)
             lgth = 4 - len(v)
             payload += b'\x00' * lgth + v
+        print("shifted message :"+ shifted_message)
         self.sock.send(payload)
         return len(payload)
 
@@ -37,37 +36,29 @@ class MySocket:
         payload = bytes("ISC", 'utf-8') + bytes(messageType, 'utf-8')
         payload += len(msg).to_bytes(2, byteorder='big')
         for p in msg:
-            v = int.from_bytes(bytes(p,"utf-8")) ^ amount
+            v = int.from_bytes(bytes(p, "utf-8")) ^ amount
             v = v.to_bytes(4)
             lgth = 4 - len(v)
             payload += b'\x00' * lgth + v
         self.sock.send(payload)
         return len(payload)
-    def decodeXorMessage(self,payload:str):
+
+    def decodeXorMessage(self, payload: str):
         pass
 
-
-    def receive(self,typeToWait='t') -> str:
-        waiting=True
+    def receive(self, typeToWait='t') -> str:
+        waiting = True
         while waiting:
-            print("a")
             data = self.sock.recv(2048)
-            print("b")
             header = data[0:3].decode("utf-8")
-            print(header, data[3:4].decode("utf-8") == typeToWait)
             if header == "ISC" and data[3:4].decode("utf-8") == typeToWait:
-                waiting=False
-        print("find a message")
+                waiting = False
+
         header = data[0:3].decode("utf-8")
         if header == "ISC":
             mode = data[3:4].decode("utf-8")
             lgth = int.from_bytes(data[4:6], "big")
             content = data[6:len(data)].decode("utf-8")
-            message = ""
-            cn = 0
-            for c in content.replace('\x00', ''):
-                if cn <= lgth:
-                    message += c
-                cn += 1
+            message = "".join(c for c in content if c != '\x00')
             return message
         return ""
