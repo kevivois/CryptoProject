a72a4720448d26d4094c8c5cf9f66e532cfedcc2
NatXiao <natasha.xiao@outlook.com>
2024-03-08 11:36:59 +0100

    vigenere + test-main file

--- Socket.py
+++ Socket.py
@@ -10,30 +10,47 @@ class MySocket:
 
     def connect(self, host, port):
         self.sock.connect((host, port))
-    def send(self, msg, messageType):
-        payload = bytes("ISC", 'utf-8') + bytes(messageType, 'utf-8')
+
+    def send(self, msg, message_type):
+        payload = bytes("ISC", 'utf-8') + bytes(message_type, 'utf-8')
         payload += len(msg).to_bytes(2, byteorder='big')
         for p in msg:
             lgth = 4 - len(bytes(p, 'utf-8'))
             payload += b'\x00' * lgth + bytes(p, 'utf-8')
         self.sock.send(payload)
         return len(payload)
+    
+    def sendvigenere(self, msg: str, message_type : str, key : str):
+        payload = bytes("ISC", 'utf-8') + bytes(message_type, 'utf-8')
+        payload += len(msg).to_bytes(2, byteorder='big')
+        vigenered_message = ""
+        for p in range(len(msg)) :
+            v = (int.from_bytes(bytes(msg[p], "utf-8"), "big") + ord(key[p%len(key)])).to_bytes(4, "big")
+            vigenered_message += chr(int.from_bytes(bytes(msg[p], "utf-8"), "big") + ord(key[p%len(key)]))
+            lgth = 4 - len(v)
+            payload += b'\x00' * lgth + v
+        self.sock.send(payload)
+        print("vigenered message :"+ vigenered_message)
+        return len(payload)
 
-    def sendshift(self, msg: str, messageType: str, amount: int):
-        payload = bytes("ISC", 'utf-8') + bytes(messageType, 'utf-8')
+    def sendshift(self, msg: str, message_type: str, amount: int):
+        payload = bytes("ISC", 'utf-8') + bytes(message_type, 'utf-8')
         payload += len(msg).to_bytes(2, byteorder='big')
         shifted_message = ""
         for p in msg:
-            v = (int.from_bytes(bytes(p, "utf-8")) + amount).to_bytes(4)
-            shifted_message += chr(int.from_bytes(bytes(p, "utf-8")) + amount)
+            v = (int.from_bytes(bytes(p, "utf-8"), "big") + amount).to_bytes(4, "big")
+            shifted_message += chr(int.from_bytes(bytes(p, "utf-8"), "big") + amount)
             lgth = 4 - len(v)
             payload += b'\x00' * lgth + v
         print("shifted message :"+ shifted_message)
         self.sock.send(payload)
         return len(payload)
+    
+    def frequenceAnalysisShift(msg : str):
+        foo = 0
 
-    def sendxor(self, msg: str, messageType: str, amount: int):
-        payload = bytes("ISC", 'utf-8') + bytes(messageType, 'utf-8')
+    def sendxor(self, msg: str, message_type: str, amount: int):
+        payload = bytes("ISC", 'utf-8') + bytes(message_type, 'utf-8')
         payload += len(msg).to_bytes(2, byteorder='big')
         for p in msg:
             v = int.from_bytes(bytes(p, "utf-8")) ^ amount
---
+++ test.py
@@ -0,0 +1,51 @@
+from Socket import MySocket
+
+def get_user_input(prompt):
+    return input(prompt).strip()
+
+def encode_message(b, cmd, messageType):
+    encode = cmd.split(" ")[0]
+
+def task_method(b, cmd, messageType):
+    code = cmd.split(" ")[1]
+    #récup des données
+    b.send(cmd, messageType)
+    msg = b.receive("s")
+    print("> " + msg)
+    key = msg.split(" ")[-1]
+    msg_to_encode = b.receive(messageType)
+    print("> " + msg_to_encode)
+    if code == "shift" : 
+        key = int(key) 
+        b.sendshift(msg_to_encode, messageType, key)
+    elif code == "vigenere" : 
+        b.sendvigenere(msg_to_encode, messageType, key)
+    else :
+        key = int(key)
+        b.sendxor(msg_to_encode, messageType, key)
+    print("> " + b.receive(messageType))
+        
+
+
+def main():
+    b = MySocket()
+    b.connect("vlbelintrocrypto.hevs.ch", 6000)
+
+    while True:
+        cmd = get_user_input("Write your command ['task'] 'shift'|'vigenere'|'xor' message:")
+
+        if cmd.split(" ")[0] == "task" : 
+            message_type = "s"
+            task_method(b, cmd, message_type)
+        else :
+            encode_message_flag = cmd.split(" ")[0] == "shift" or "vigenere" or "xor"
+            if encode_message_flag:
+                encode_message(b, cmd, message_type)
+            else:
+                b.send(cmd, message_type)
+                response = b.receive(message_type)
+                print("<" + response)
+
+
+if __name__ == "__main__":
+    main()
